f9dbade941af1b2d75a1a1b08e6a8472fcedc3de
SAIF879 <saifahmed9308@gmail.com>
2023-06-16 12:48:19 +0530

    Initial commit

---
+++ src/conceptSheets/Searching1.java
@@ -0,0 +1,39 @@
+package conceptSheets;
+
+public class Searching1 {
+    public static void main(String[] args) {
+
+        //BINARY SEARCH
+
+        //COUNT OCCURRENCE IN SORTED ARRAY
+        //NAIVE
+        //EFFECTIVE
+
+        //COUNT NO OF 1'S IN SORTED ARRAY
+        //NAIVE
+        //EFFECTIVE
+
+        //SQUARE ROOT OF AN INTEGER
+
+        //SEARCH IN INFINITE SIZED ARRAY
+
+        //SEARCH IN SORTED ROTATED ARRAY
+
+        //FIND PEAK ELEMENT
+
+        //FIND PAIRS WITH GIVEN SUM
+
+        //REPEATING ELEMENTS
+
+        //ALLOCATE MINIMUM PAGES**
+
+        //MEDIAN OF 2 SORTED ARRAYS WITH DIFFERENT SIZE**
+
+        //MAJORITY ELEMENT
+
+        //COUNT ONLY REPEATED
+
+        //ROOF TOP
+
+    }
+}
---
+++ src/conceptSheets/Sorting1.java
@@ -0,0 +1,7 @@
+package conceptSheets;
+
+public class Sorting1 {
+    public static void main(String[] args) {
+
+    }
+}
