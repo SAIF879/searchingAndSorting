19709e0cd10fb2d53912484fa39252bd4c26e050
SAIF879 <saifahmed9308@gmail.com>
2023-06-16 17:22:29 +0530

    binary search

--- src/conceptSheets/Searching1.java
+++ src/conceptSheets/Searching1.java
@@ -1,11 +1,32 @@
 package conceptSheets;
 
 public class Searching1 {
+
+    public static int binarySearch(int arr[], int number){
+        int length = arr.length;
+        int start = 0 ;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start+end/2;
+            if (arr[mid]==number) return mid;
+            else if(arr[mid]<number){start = mid+1;}
+            else end = mid -1;
+        }
+        return -1;
+    } //o(logN) , no aux space
+
     public static void main(String[] args) {
 
         //BINARY SEARCH
 
+        // int arr[] = {10 ,20 , 30 , 40 , 50 , 60};
+        //int x = 40;
+        //System.out.println(binarySearch(arr, 20));
+
+
         //COUNT OCCURRENCE IN SORTED ARRAY
+
+
         //NAIVE
         //EFFECTIVE
 
